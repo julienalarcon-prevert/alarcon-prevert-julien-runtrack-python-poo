74eda6a986cd9af687c2bae5daeb2c65913381cd
Julien-13 <julien.alarcon-prevert@laplateforme.io>
2026-02-02 15:46:34 +0100

    Job 5

--- job01.py
+++ job01.py
@@ -1,11 +1,25 @@
-class Personne:
-    def __init__(self, nom, prenom):
-        self.nom = nom
-        self.prenom = prenom
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
         
-    def SePresenter(self):
-        return f"je m'appelle {self.nom} {self.prenom}"
-        
-personne1 = Personne("Alarcon prevert", "Julien")  
+    def afficherpoints(self):
+        print(f"Cooordonnées : ({self.x}, {self.y})")
+    
+    def prtX(self):
+        print(f"Coordonnées de X : {self.x}")
+    
+    def prtY(self):
+        print(f"Coordonnées de Y : {self.y}")
+    
+    def newX(self, newX):
+        self.x = newX
 
-print(personne1.SePresenter())
+    def newY(self, newY):
+        self.x = newY
+        
+point1 = Point(10, 20)
+point1.afficherpoints()
+
+point1.newX(50)
+point1.prtX()
