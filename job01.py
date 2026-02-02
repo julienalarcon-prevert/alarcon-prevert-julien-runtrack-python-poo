3012bdfe33a5d685a390fd9da4b4044a7df58784
Julien-13 <julien.alarcon-prevert@laplateforme.io>
2026-02-02 18:08:51 +0100

    job 8

--- job01.py
+++ job01.py
@@ -1,32 +1,24 @@
-class Personnage():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def gauche(self):
-        self.x -= 1
-    
-    def droite(self):
-          self.x += 1
-    
-    def haut(self):
-        self.y -= 1
-        
-    def bas(self):
-        self.y += 1
-    
-    def position(self):
-        return(self.x, self.y)
-    
-perso = Personnage(10, 10)
+import math
 
-print(perso.position())
+class Cercle:
+    def __init__(self, rayon):
+        self.rayon = rayon
+    
+    def changer_rayon(self, new_rayon):
+        self.rayon = new_rayon
+    
+    def afficher_infos(self):
+        print(f"Cercle de rayon : {self.rayon}")
+    
+    def diametre(self):
+        return self.rayon * 2
+    
+    def circonference(self):
+        return 2 * math.pi * self.rayon
 
-perso.droite()
-perso.bas()
-print(perso.position())
-
-perso.gauche()
-perso.gauche()
-perso.haut()
-print(perso.position())
+    def aire(self):
+        return math.pi * (self.rayon * self.rayon)
+    
+    
+cercle1 = Cercle(4)
+cercle2 = Cercle(7)
