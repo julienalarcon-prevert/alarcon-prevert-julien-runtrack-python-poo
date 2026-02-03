049fc9c2b4f4a7e641e545a28ca2ea024d7e9ad9
Julien-13 <julien.alarcon-prevert@laplateforme.io>
2026-02-03 17:00:46 +0100

    Job 2 / day2

--- day2/job01.py
+++ day2/job01.py
@@ -1,18 +1,27 @@
-class Rectangle: 
-    def __init__(self, longueur, largeur):
-        self.__longueur = longueur
-        self.__largeur = largeur
+class Livre:
+    def __init__(self, titre, auteur, nbpages):
+        self.__titre = titre
+        self.__auteur = auteur
+        self.__nbpages = nbpages
         
-    def get_longueur(self):
-        return self.__longueur
+    def get_titre(self):
+        return self.__titre
     
-    def set_long(self, value):
-        if value > 0:
-            self.__longueur = value
+    def get_auteur(self):
+        return self.__auteur
+    
+    def get_nbpages(self):
+        return self.__nbpages
+    
+    def set_titre(self, nwtitre):
+        self.__titre = nwtitre
+    
+    def set_auteur(self, nwauteur):
+        self.__auteur = nwauteur
+    
+    def set_nbpages(self, nwnbpages):
+        if isinstance(nwnbpages, int) and nwnbpages > 0:
+            self.__nbpages = nwnbpages
+        else:
+            print("Error : Must use integer")
         
-    def get_largeur(self):
-        return self.__largeur
-    
-    def set_largeur(self, value):
-        if value > 0:
-            self.__largeur = value
