b6e163d6a61eee4716c40481db7c967b2a69b870
Julien-13 <julien.alarcon-prevert@laplateforme.io>
2026-02-05 11:51:28 +0100

    Job01 / day 4

---
+++ day4/job01.py
@@ -0,0 +1,29 @@
+class Personne:
+    def __init__(self, age = 14):
+        self.age = age
+        
+    def get_age(self):
+        print (self.age)
+        
+    def bonjour(self):
+        print("Hello")
+    
+    def set_age(self, value):
+        self.age = value
+
+class Eleve(Personne):
+    def __init__(self):
+        super().__init__(age=14)
+    
+    def aller_en_cours(self):
+        print("Je vais en cours")
+        
+    def afficher_age(self):
+        print(f"J'ai {self.age} ans ")
+
+class Professeur:
+    def __init__(self, matiere):
+        self.__matiere = matiere
+    
+    def enseigner(self):
+        print("Le cours va commencer")
